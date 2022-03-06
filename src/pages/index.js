cab2bcae9519282f80a06a2a915d11129ef557aa
AdamWarfield <anw5304@gmail.com>
2022-03-06 15:39:11 -0500

    Add import

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { graphql } from 'gatsby'
 import get from 'lodash/get'
 
 import Layout from '../components/layout'
-import Hero from '../components/hero'
+import portfolio_hero from '../components/portfolio-hero'
 import ArticlePreview from '../components/article-preview'
 
 class RootIndex extends React.Component {
