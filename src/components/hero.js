0de98a86f06245cf111dbfc7b01b35a2a182c3b4
AdamWarfield <anw5304@gmail.com>
2022-03-06 20:13:21 -0500

    Night sky bg

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,19 +4,18 @@ import { GatsbyImage } from 'gatsby-plugin-image'
 import * as styles from './hero.module.css'
 
 const Hero = ({ image, name, title, content }) => (
-  <section class="blue-bg">
+  
   <div className={styles.hero}>
     {image && (
       <GatsbyImage className={styles.image} alt={image.description} image={image} />
     )}
     
     <div className={styles.details}>
-      <h1 className={styles.name}>{name}<small className='style.title'>{title}</small></h1>
+      <h1 className={styles.name}>{name} <br></br><small className='style.title'>{title}</small></h1>
       {content && <p className={styles.content}>{content}</p>}
     </div>
     
   </div>
-  </section>
 )
 
 export default Hero
