aab761df8b36a944b856f095bd358825a58b8aa4
AdamWarfield <anw5304@gmail.com>
2022-04-09 17:06:11 -0400

    weighted random stars

--- static/scripts.js
+++ static/scripts.js
@@ -1,21 +1,26 @@
 function stars(){
   
-    function freshDot(){
-      var container = document.querySelector('.star-container');
-      this.obj = document.createElement("div");
-      this.obj.classList.add("star");
-      this.obj.style.top = (container.offsetHeight * Math.random()) + 'px';
-      this.obj.style.left = (container.offsetWidth * Math.random()) + 'px';
-      this.size = Math.floor(3 * Math.random()) + 7;
-      this.obj.style.height =  this.size + 'px';
-      this.obj.style.width = this.size + 'px';
-      
-      container.appendChild(this.obj);
+  function starGenerator(){
+    var container = document.querySelector('.star-container');
+    this.obj = document.createElement("div");
+    this.obj.classList.add("star");
+    if (Math.random() > .33 ){
+      this.obj.style.top = ((Math.random() * (0 - 33) +33) + '%');
+    }else{
+      this.obj.style.top = ((Math.random() * (100 - 32) +32) + '%');
     }
-    var dot = [];
-    for(var i = 0 ; i < 200; i++ ){
-      dot.push(new freshDot());
-    }
+    this.obj.style.left = (Math.random() * 100 + '%');
+    this.obj.style.opacity =(Math.random() * ( .9 - .66) + .66);
+    this.size = Math.floor(3 * Math.random()) + 7;
+    this.obj.style.height =  this.size + 'px';
+    this.obj.style.width = this.size + 'px';
+    
+    container.appendChild(this.obj);
+  }
+  var dot = [];
+  for(var i = 0 ; i < 200; i++ ){
+    dot.push(new starGenerator());
+  }
 
   };
   
