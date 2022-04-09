d1e4c4a1051aaf36d1989bdb96f8824c0bb88971
AdamWarfield <anw5304@gmail.com>
2022-04-09 19:54:35 -0400

    Fix strings

--- src/components/hero.js
+++ src/components/hero.js
@@ -13,17 +13,14 @@ const Hero = ({ image, name, title, content, linkedin, email }) => (
     <div className={styles.details}>
       <h1 className={styles.name}>{name} <br></br><small className='style.title'>{title}</small></h1>
       {content && <p className={styles.content}>{content}</p>}
-      <div className={styles.contact}>
         
         <div className={styles.contact}>
           
-          <a href='${linkedin}'>linkedIn</a>
-          <a href='mailto:${email}'>{email}</a>
+          <a href={`${linkedin}`}>linkedIn</a>
+          <a href={`mailto:${email}`}>{email}</a>
           </div>
       </div>
     </div>
-    
-  </div>
 )
 
 export default Hero
